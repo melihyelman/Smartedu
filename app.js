5d7cf7bfd5ad320a73f6747005bd26743ab7be3a
melihyelman <myelman17@gmail.com>
2021-08-04 01:04:49 +0300

    add course model

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
 
+//Connect Db
+mongoose
+  .connect('mongodb://localhost/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('DB connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 // Template Engine
 app.set('view engine', 'ejs');
 
@@ -11,6 +28,7 @@ app.use(express.static('public'));
 
 //Routes
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
