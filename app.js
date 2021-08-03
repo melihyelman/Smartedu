e4681357bf916f8422a9591662cb662e90bb3a5d
melihyelman <myelman17@gmail.com>
2021-08-04 00:22:12 +0300

    create workspace

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+
+//Routes
+app.get('/', (req, res) => {
+  res.status(200).render('index', {
+    pageName: 'index',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    pageName: 'about',
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
