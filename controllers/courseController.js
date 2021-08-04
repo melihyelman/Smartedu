75ca3cc0e8ec7fdc00300c0e7c35675c9931e15a
melihyelman <myelman17@gmail.com>
2021-08-04 16:49:15 +0300

    list courses

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,14 +1,30 @@
 const Course = require('../models/Course');
 
 exports.createCourse = async (req, res) => {
-  const course = await Course.create(req.body);
-
   try {
+    const course = await Course.create(req.body);
+
     res.status(201).json({
       status: 'Success',
       course,
     });
-  } catch {
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find().sort('-createdAt');
+
+    res.status(200).render('courses', {
+      courses,
+      pageName: 'courses',
+    });
+  } catch (error) {
     res.status(400).json({
       status: 'fail',
       error,
