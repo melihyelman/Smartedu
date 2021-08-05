33949c29d115d48097f4cf2773f9c6985f96f9c1
melihyelman <myelman17@gmail.com>
2021-08-05 15:07:38 +0300

    add login page and session

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const bcrpyt = require('bcrypt');
+
 const User = require('../models/User');
 
 exports.createUser = async (req, res) => {
@@ -7,6 +9,29 @@ exports.createUser = async (req, res) => {
     res.status(201).json({
       status: 'Success',
       user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    await User.findOne({ email }, (err, user) => {
+      if (user) {
+        bcrpyt.compare(password, user.password, (err, same) => {
+          if (same) {
+            //user sessions
+            req.session.userId = user._id;
+            res.status(200).redirect('/');
+          }
+        });
+      }
     });
   } catch (error) {
     res.status(400).json({
