e5db3bf254178ea230dd25f2136f306116b5dc60
melihyelman <myelman17@gmail.com>
2021-08-06 17:42:19 +0300

    add login and register validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrpyt = require('bcrypt');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/User');
 const Category = require('../models/Category');
@@ -10,10 +11,13 @@ exports.createUser = async (req, res) => {
 
     res.status(201).redirect('/login');
   } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      error,
+    const errors = validationResult(req);
+
+    errors.array().map((error) => {
+      req.flash('error', `${error.msg} <br />`);
     });
+
+    res.status(400).redirect('/register');
   }
 };
 
@@ -25,9 +29,17 @@ exports.loginUser = async (req, res) => {
       if (user) {
         bcrpyt.compare(password, user.password, (err, same) => {
           //user sessions
-          req.session.userId = user._id;
-          res.status(200).redirect('/users/dashboard');
+          if (same) {
+            req.session.userId = user._id;
+            res.status(200).redirect('/users/dashboard');
+          } else {
+            req.flash('error', 'Your password is not correct');
+            res.status(400).redirect('/login');
+          }
         });
+      } else {
+        req.flash('error', 'User is not exist!');
+        res.status(400).redirect('/login');
       }
     });
   } catch (error) {
